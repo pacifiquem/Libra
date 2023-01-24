8d231ddb65ad9334b92bdd65fdff416b5bb0b6bb
ndzhwr <ndizihiweregis06@gmail.com>
2023-01-24 11:02:19 +0200

    feat: account class

--- src/main/java/com/Libra/orm/Account.java
+++ src/main/java/com/Libra/orm/Account.java
@@ -18,4 +18,52 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
-public class Account {}
+
+@Entity(name = "account")
+public class Account {
+    @Id
+     @Column
+     @GeneratedValue(strategy = GenerationType.UUID)
+     private int id;
+
+     @Column
+     private  String username;
+
+     @Column
+     private  String email ;
+
+     @Column
+     private  String password;
+
+ public int getId() {
+  return id;
+ }
+
+ public void setId(int id) {
+  this.id = id;
+ }
+
+ public String getUsername() {
+  return username;
+ }
+
+ public void setUsername(String username) {
+  this.username = username;
+ }
+
+ public String getEmail() {
+  return email;
+ }
+
+ public void setEmail(String email) {
+  this.email = email;
+ }
+
+ public String getPassword() {
+  return password;
+ }
+
+ public void setPassword(String password) {
+  this.password = password;
+ }
+}
